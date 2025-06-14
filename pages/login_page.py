39e44a7ae051fa2b883f2d58f3dd25a14d678b7f
Sergey Alekseev <sergey.alekseev.46.46@gmail.com>
2025-06-14 13:48:32 +0300

    реализован Page Object для страницы логина и проверки её элементов
    - создан класс LoginPageLocators с селекторами для форм логина и регистрации
    - добавлены методы should_be_login_url, should_be_login_form, should_be_register_form в LoginPage
    - реализована проверка наличия подстроки "login" в текущем URL
    - подготовлена база для перехода между страницами и проверки элементов на странице логина

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        print(f">>> Current URL in should_be_login_url: {self.browser.current_url}")
+        assert LoginPageLocators.LOGIN_URL in self.browser.current_url, "Login substring is not present in the current URL"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register is not presented"
